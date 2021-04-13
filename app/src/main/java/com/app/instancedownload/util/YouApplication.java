8edd99119d33bf6fd140c6e866d2b91aae42d916
Kishan Viramgama <viramgama.kishan@gmail.com>
2021-04-13 22:26:51 +0530

    Initial release

---
+++ app/src/main/java/com/app/instancedownload/util/YouApplication.java
@@ -0,0 +1,18 @@
+package com.app.instancedownload.util;
+
+import android.app.Application;
+import android.os.StrictMode;
+
+public class YouApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+        builder.detectFileUriExposure();
+
+    }
+
+}
